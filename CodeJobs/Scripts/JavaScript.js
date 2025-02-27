0e9b0b83190dcc6b7628f96010545549c2cf4166
laur Botnari <laurentiu2482@gmail.com>
2025-02-27 18:00:34 +0200

    starting on side_bar

---
+++ CodeJobs/Scripts/JavaScript.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("sidebar");
+    const toggleBtn = document.getElementById("sidebarToggle");
+
+    toggleBtn.addEventListener("click", function () {
+        sidebar.classList.toggle("sidebar-collapsed");
+    });
+
+    document.querySelectorAll(".has-submenu > a").forEach(item => {
+        item.addEventListener("click", function (e) {
+            e.preventDefault();
+            this.parentElement.classList.toggle("open");
+            this.nextElementSibling.classList.toggle("active");
+            this.querySelector(".arrow").classList.toggle("rotate");
+        });
+    });
+});
