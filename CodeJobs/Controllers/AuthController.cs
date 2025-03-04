cd7e2f96418b9ecc6b1406f3df586df02839b7a1
laur Botnari <laurentiu2482@gmail.com>
2025-03-04 17:35:20 +0200

    registration page

---
+++ CodeJobs/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using CodeJobs.Models;
+using System.Web.Mvc;
+using CodeJobs.Models; // Import the RegisterViewModel namespace
+
+namespace YourNamespace.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: /Auth/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Auth/Register
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Registration logic (e.g., save the user to the database)
+                // If registration is successful, redirect to the Home page or another page
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            // If model validation fails, return the view with validation errors
+            return View(model);
+        }
+    }
+}
---
+++ CodeJobs/Models/registerViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeJobs.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
