803acac2c635eba35ad9450f9ae6d0a3ce320f87
Alexandru Chirica <mamorusan03@gmail.com>
2025-03-11 08:41:22 -0700

    Add views "Home"

---
+++ CodeJobs/controllers/HomeController_A.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+}
