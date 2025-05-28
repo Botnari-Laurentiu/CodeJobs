2d405cee15e242198acb016f04ee094fef84dc7f
Alexandru Chirica <mamorusan03@gmail.com>
2025-05-28 14:48:19 -0700

    Add information form data base about job in container in JobList

---
+++ CodeJobs/Controllers/JobController.cs
@@ -0,0 +1,26 @@
+﻿using CodeJobs.Business_Logic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CodeJobs.Controllers
+{
+    public class JobsController : Controller
+    {
+        private readonly IJobPostService _jobPostService;
+
+        public JobsController()
+        {
+            _jobPostService = new JobPostingService();
+        }
+
+        public async Task<ActionResult> JobsList()
+        {
+            var jobs = await _jobPostService.GetAllJobPosts();
+            return View(jobs);
+        }
+    }
+}
